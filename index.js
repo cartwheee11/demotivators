b02f01115870464c0444673756b8852c55c33f19
Cartwheel <cartwheel@MBP-Cartwheel.Dlink>
2021-06-23 14:53:57 +0300

    dunno

--- index.js
+++ index.js
@@ -11,13 +11,31 @@ Canvas.registerFont('fonts/Roboto-Regular.ttf', { family: 'kek-Arial' });
 let client = new Discord.Client();
 client.login(config.BOT_TOKEN);
 
+const NO_IMAGE_ERRORS = [
+	'Вы не прикрепили картинку, обязательно сделайте это!',
+	'У вас что, нет картинки? Сбросьте мне по почте',
+	'Эмм, а картинка?',
+	'Вы забыли про картинку'
+]
+
+const INCORRECT_ATTACHMENT_ERRORS = [
+	'Вместо картики вы скинули что-то не то',
+	'Мы ждем именно картинку, а не непонятный файл'
+]
+
 const HELLOS = [
 	'Привет, вот твой дивный демотиватор:',
 	'Как же крут твой новый демотиватор:',
-	'дИИИИИмотиватор готов:',
-	'кек',
-	'лол'
+	'Деееееемотиватор готов:',
+	'Кек',
+	'Лол'
 ]
+
+function getRandomArrayElement(arr) {
+	let len = arr.length;
+	let rand = Math.floor(Math.random() * len);
+	return arr[rand];
+}
 
 function getRandHello(arr) {
 	let len = arr.length;
@@ -101,37 +119,79 @@ client.on('message', message => {
 	try{
 		if(!message.content.startsWith(PREFIX)) return;
 		if(message.author.bot) return;
-		
+		let textBody = message.content.replace(PREFIX, '').trim();
+		if(textBody == '') {
+			message.delete();
+			message.reply('Нет текста — нет мема');
+			return;
+		}
+		//получаем текст из сообщения
+		let tmp = textBody.split('|');
+		tmp = tmp.map(elem => { elem.trim(); if (elem != '') return elem });
+		let text = [];
+
+		tmp.forEach(elem => {
+			if(elem) {
+				elem = elem.trim();
+				if(elem != '')
+					text.push(elem);
+			}
+			
+		});
+
+		if(text.length > 2) {
+			for(let i = 2; i < text.length; i++) {
+				text[1] += (' | ' + text[i]);
+			}
+		}
+
+		if(!message.attachments.size)  {
+			message.delete();
+			message.reply(getRandomArrayElement(NO_IMAGE_ERRORS));
+			return;
+		}
+
+		let attachment = Array.from(message.attachments)[0][1];
+		if(!attachment.height || !attachment.width) {
+			message.delete();
+			message.reply(getRandomArrayElement(INCORRECT_ATTACHMENT_ERRORS));
+			return;
+		}
+
 		//получаем картинку из вложения
 		let obtainedImageURL = Array.from(message.attachments)[0][1].attachment;
-	
+
 		//создаем и настраиваем элемент Canvas
 		let canvas = Canvas.createCanvas();
 		let c = canvas.getContext('2d');
 		canvas.width = 500;
 		
-		//получаем текст из сообщения
-		let text = message.content.replace(PREFIX, '').trim().split('|');
-		text = text.map(elem => elem.trim());
 		Canvas.loadImage(obtainedImageURL).then(obtainedImage => {
 			
 			message.delete();
 
 			c.font = TITLE_FONT;
 			let title = text[0];
-			// console.log(title);
 			let titleHeight = getCanvasTextareaHeight(c, title, CONTENT_WIDTH, TITLE_LINE_HEIGHT);
-	
-			c.font = SUBTITLE_FONT;
-			let subtitle = text[1];
-			// console.log(subtitle);
-			let subtitleHeight = getCanvasTextareaHeight(c, subtitle, CONTENT_WIDTH, SUBTITLE_LINE_HEIGHT);
-			let textHegiht = subtitleHeight + titleHeight;
+
+			let textHeight = 0;
+			let subtitleHeight = 0;
+			let subtitle = null;
+
+			if(text[1]) {
+				c.font = SUBTITLE_FONT;
+				subtitle = text[1];
+				subtitleHeight = getCanvasTextareaHeight(c, subtitle, CONTENT_WIDTH, SUBTITLE_LINE_HEIGHT);
+				
+			} 
+
+			textHegiht = subtitleHeight + titleHeight;
+			
 			let formatedImageWidth = canvas.width - CONTAINER_PADDING * 2;
 			let sizeMultipler = 1 / obtainedImage.width * formatedImageWidth;
 			let formatedImageHeight = obtainedImage.height * sizeMultipler;
 			
-			canvas.height = formatedImageHeight + CONTAINER_PADDING * 2 + textHegiht + TEXT_MARGIN_TOP * 2;
+			canvas.height = formatedImageHeight + CONTAINER_PADDING * 2 + textHegiht + TEXT_MARGIN_TOP * ( subtitle ? 2 : 1);
 	
 			c.textAlign = 'center';
 			c.textBaseline = 'top';
@@ -148,15 +208,18 @@ client.on('message', message => {
 			c.fillStyle = 'white';
 			c.font = TITLE_FONT;
 			printCanvasTextarea(c, title, canvas.width / 2, CONTAINER_PADDING + formatedImageHeight + TEXT_MARGIN_TOP, CONTENT_WIDTH, TITLE_LINE_HEIGHT);
-	
-			c.font = SUBTITLE_FONT;
-			printCanvasTextarea(c, subtitle, canvas.width / 2, CONTAINER_PADDING + formatedImageHeight + TEXT_MARGIN_TOP * 2 + titleHeight, CONTENT_WIDTH, SUBTITLE_LINE_HEIGHT);
+			
+			if(subtitle){
+				c.font = SUBTITLE_FONT;
+				printCanvasTextarea(c, subtitle, canvas.width / 2, CONTAINER_PADDING + formatedImageHeight + TEXT_MARGIN_TOP * 2 + titleHeight, CONTENT_WIDTH, SUBTITLE_LINE_HEIGHT);
+			}
 	
 			let replyText = getRandHello(HELLOS);
 			message.reply(replyText, { files: [ canvas.toBuffer() ] })
 		});
 	} catch {
-		message.reply('Это так не работает');
+		message.reply('Некорректная команда');
+		message.delete();
 	}
 
 });
